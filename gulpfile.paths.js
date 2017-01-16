86be19eec4156cc566085b37aa2c3b149f2ec63e
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-01-16 18:43:38 +0100

    init reestructuration

--- gulpfile.paths.js
+++ gulpfile.paths.js
@@ -1,63 +1,53 @@
-module.exports = {
+let paths = {
     port: 4000,
-    
+
     app: {
-        _folder: './app',
-
         assets: {
-           _folder: './app/assets',
+            images: {},
+            fonts: {}
+        },
 
-           images: {
-               _folder: './app/assets/images',
-               _files: './app/assets/images/**/*.*',
-           },
+        scripts: {
+            _files: '**/*.js',
+            _folder: 'js',
+        },
 
-           fonts: {
-               _folder: './app/assets/fonts',
-               _files: './app/assets/fonts/**/*.*'
-           }
-       },
-
-       scripts: {
-            _files: './app/js/**/*.js',
-            _folder: './app/js',
-
-            main: {
-                _files: './app/js/app.js',
-                _folder: './app/js',
-            },
+        main: {
+            _files: 'app.js',
+            _folder: 'js',
         },
 
         scss: {
-            _files: './app/scss/**/*.scss',
-            _folder: './app/scss',
+            _files: '**/*.scss',
         }
     },
 
     dist: {
-       _files: './dist/*',
-       _folder: './dist',
-
-       assets: {
-           _folder: './dist/assets',
-
-           images: {
-               _folder: './dist/assets/images',
-               _files: './dist/assets/images/**/*.*',
-           },
-
-           fonts: {
-               _folder: './dist/assets/fonts',
-               _files: './dist/assets/fonts/**/*.*',
-           }
-       },
-
-       css: {
-           _folder: './dist/css'
-       },
-
-       js: {
-           _folder: './dist/js'
-       }
+        assets: {
+            images: {},
+            fonts: {}
+        },
+        css: {},
+        js: {}
     }
 };
+
+let parseLevel = (obj, parent) => {
+    if (parent) {
+        obj._files = obj._files || '**/*.*'
+        obj._folder = obj._folder || parent;
+    }
+
+    Object.keys(obj).forEach(key => {
+        if (typeof obj[key] === 'object') {
+            parseLevel(obj[key], key);
+            obj[key]._parent = parent;
+        }
+    });
+};
+
+let parsePaths = () => {
+ parseLevel(paths);
+};
+
+parsePaths();
