06b6beaa938ce7ba6967ebf5cdbbd68dab78fa28
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-03-13 13:33:42 +0100

    Change to .vue before try new lib

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.component'
 
+// mount
 new Vue({
-  el: '#app',
+  el: '#app2',
   render: h => h(App, {
     props: { propMessage: 'World' }
   })
--- tasks/ts.js
+++ tasks/ts.js
@@ -1,17 +1,26 @@
 const rollup = require('rollup');
 const typescript = require('rollup-plugin-typescript');
 const inject = require('rollup-plugin-inject');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const replace = require('rollup-plugin-replace');
+const vue = require('rollup-plugin-vue');
 
 module.exports = (gulp, paths, $, _) => {
     return rollup.rollup({
         entry: './app/ts/app.ts',
         plugins: [
+            vue({compileTemplate: true}),         
             typescript({
                 typescript: require("typescript")
             }),
-            inject({
-                include: '**/*.js',
-                Vue: 'vue',
+            nodeResolve({ jsnext: true,
+              main: true,
+              browser: true 
+            }),
+            commonjs(),
+            replace({
+                'process.env.NODE_ENV': '\"production\"'
             }),
         ],
     })
