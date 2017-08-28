a666cee3ddc5e51e2fff9e00f3e78d20fbde3ec7
Quique Fdez Guerra <quique@ckgrafico.com>
2017-08-29 01:06:04 +0200

    Change routes to array

--- app/scripts/app.router.ts
+++ app/scripts/app.router.ts
@@ -14,7 +14,7 @@ export function Router() {
                 path: '/',
                 redirect: '/cities',
             },
-            citiesRoutes
+            ...citiesRoutes
         ]
     });
 
--- app/scripts/cities/cities.routes.ts
+++ app/scripts/cities/cities.routes.ts
@@ -1,26 +1,28 @@
 import { RouteConfig } from 'vue-router';
 
-export let citiesRoutes: RouteConfig = {
-    path: '/cities',
-    name: 'cities',
-    component: () => import('./cities.component'),
-    children: [
-        {
-            path: '',
-            name: 'city-list',
-            component: () => import('./city-list/city-list.component')
-        },
-        // {
-        //     path: '/:id',
-        //     name: 'city-detail',
-        //     component: CityDetailComponent,
-        //     props: true
-        // },
-        // {
-        //     path: '/:id/edit',
-        //     name: 'city-edit',
-        //     component: CityEditComponent,
-        //     props: true
-        // }
-    ]
-};
+export let citiesRoutes: RouteConfig[] = [
+    {
+        path: '/cities',
+        name: 'cities',
+        component: () => import('./cities.component'),
+        children: [
+            {
+                path: '',
+                name: 'city-list',
+                component: () => import('./city-list/city-list.component')
+            },
+            // {
+            //     path: '/:id',
+            //     name: 'city-detail',
+            //     component: CityDetailComponent,
+            //     props: true
+            // },
+            // {
+            //     path: '/:id/edit',
+            //     name: 'city-edit',
+            //     component: CityEditComponent,
+            //     props: true
+            // }
+        ]
+    }
+];
