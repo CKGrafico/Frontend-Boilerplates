59ff7720bf91ec1e73678eb470e9bc6b3a140681
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-08-29 21:51:39 +0200

    Add reflect

--- app/scripts/vendor.ts
+++ app/scripts/vendor.ts
@@ -3,8 +3,9 @@ import 'babel-polyfill';
 import 'reflect-metadata';
 
 // Vue
+import 'vue';
 import 'vue-property-decorator';
 
 // 3rd party
-import 'inversify';
 import 'localforage';
+import 'inversify';
