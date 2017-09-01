6cf26a3c852041ff2a9fdbf567eafc180a7508b4
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-09-01 17:43:21 +0200

    Add map

--- app/scripts/cities/city-detail/city-detail.component.ts
+++ app/scripts/cities/city-detail/city-detail.component.ts
@@ -12,7 +12,11 @@ export class CityDetailComponent extends BaseComponent {
     public city: City = null;
 
     @Prop()
-    public id: number; 
+    public id: number;
+
+    get mapUrl() {
+        return `https://www.bing.com/maps/embed?h=400&w=400&cp=${this.city.centroid[0]}~${this.city.centroid[1]}&lvl=12&typ=s&sty=r&src=SHELL&FORM=MBEDV8`;
+    }
 
     public async created() {
         this.citiesService = container.get<ICitiesService>(ICitiesServiceId);
