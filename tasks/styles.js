9c8d94907170e06478efcc6005f60ab81e798f9f
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-02-13 13:11:07 +0100

    Add aliases for SCSS #40

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,6 +1,9 @@
 module.exports = (gulp, paths, $, _) => {
     return gulp.src(_.files(paths.app.styles))
         .pipe($.environment.if.development($.sourcemaps.init()))
+        .pipe($.styleAliases({
+            '~': './app/styles'
+        }))
         .pipe($.sass().on('error', $.sass.logError))
         .pipe($.autoprefixer({ browsers: ['last 2 versions'] }))
         .pipe($.environment.if.production($.cleanCss()))
