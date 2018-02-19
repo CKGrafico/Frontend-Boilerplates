9f1f8c3fcb2ef8e2b96488668209a1ea44311caf
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-02-19 11:37:16 +0100

    Add stylelint

--- tasks/styles-lint.js
+++ tasks/styles-lint.js
@@ -1,6 +1,12 @@
 module.exports = (gulp, paths, $, _) => {
+    const options = {
+        failAfterError: false,
+        reporters: [
+            { formatter: 'verbose', console: true }
+        ]
+    };
+
     return gulp.src(_.files(paths.app.styles))
-        .pipe($.sassLint())
-        .pipe($.sassLint.format())
-        .pipe($.sassLint.failOnError())
+        .pipe($.environment.if.development($.stylelint(options)))
+        .pipe($.environment.if.production($.stylelint({ ...options, failAfterError: true })))
 };
