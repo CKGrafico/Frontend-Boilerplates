549e37ac04f96e6aad7ace7cc4296d19efd72d5a
Quique Fdez Guerra <quique@ckgrafico.com>
2018-03-11 18:49:17 +0100

    Fix #42

--- tasks/styles-lint.js
+++ tasks/styles-lint.js
@@ -2,11 +2,12 @@ module.exports = (gulp, paths, $, _) => {
     const options = {
         failAfterError: false,
         reporters: [
-            { formatter: 'verbose', console: true }
+            { formatter: 'string', console: true }
         ]
     };
 
     return gulp.src(_.files(paths.app.styles))
+        .pipe($.environment.if.local($.stylelint(options)))
         .pipe($.environment.if.development($.stylelint(options)))
         .pipe($.environment.if.production($.stylelint({ ...options, failAfterError: true })))
 };
