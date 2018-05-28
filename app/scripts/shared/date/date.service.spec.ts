c282a8f220e1f88dafcc07d69ff53e6d06f1a591
Juan Carlos López <jcarloslr10>
2018-05-28 11:07:22 +0200

    Fixed Mocha unit tests

--- app/scripts/shared/date/date.service.spec.ts
+++ app/scripts/shared/date/date.service.spec.ts
@@ -12,12 +12,12 @@ describe('DateService', () => {
 
     describe('when format Date', () => {
         it('should return date in correct format', () => {
-            let date = dateService.formatDate('01', '01', '2018');
+            let date = dateService.formatDate(new Date(2018, 0, 1));
             expect(date).to.equal('01/01/2018');
         });
 
         it('should return empty string if some of parameters is false', () => {
-            let date = dateService.formatDate(null, '01', '2018');
+            let date = dateService.formatDate(null);
             expect(date).to.equal('');
         });
     });
