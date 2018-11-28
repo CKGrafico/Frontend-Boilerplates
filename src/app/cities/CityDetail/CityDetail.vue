dffc083ef050e51bc3f84f597e538e1d9187a4f2
Quique Fdez Guerra <quique@ckgrafico.com>
2018-11-28 22:13:20 +0100

    Update packages

--- src/app/cities/CityDetail/CityDetail.vue
+++
@@ -1,3 +0,0 @@
-<script src="./CityDetail.vue.ts" lang="ts"></script>
-<style src="./CityDetail.vue.scss" lang="scss"></style>
-<template src="./CityDetail.vue.html" lang="html"></template>
--- src/app/weather/WeatherList/WeatherList.vue
+++
@@ -1,3 +0,0 @@
-<script src="./WeatherList.vue.ts" lang="ts"></script>
-<style src="./WeatherList.vue.scss" lang="scss"></style>
-<template src="./WeatherList.vue.html" lang="html"></template>
