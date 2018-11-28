7fdbddd8fee332166049874ff38ba8282603c615
Quique Fdez Guerra <quique@ckgrafico.com>
2018-11-28 22:30:35 +0100

    Add gulp plugin to avoid cache

--- tasks/gulp/copy.js
+++ tasks/gulp/copy.js
@@ -1,4 +1,7 @@
 module.exports = (gulp, paths, $, _) => {
+    const version = `d=${Date.now()}`;
+
     return gulp.src(_.folder(paths.src) + '/index.html')
+        .pipe($.stringReplace('{version}', version))
         .pipe(gulp.dest(_.folder(paths.dist)));
 };
