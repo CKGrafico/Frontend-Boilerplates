5557f691da1242aa2fc7cc9236a3fc668be2bf87
Quique Fdez Guerra <quique@ckgrafico.com>
2018-11-29 21:39:04 +0100

    Fix path

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,7 +1,7 @@
 module.exports = (gulp, paths, $, _) => {
     const version = `d=${Date.now()}`;
 
-    return gulp.src(_.folder(paths.src) + '/index.html')
+    return gulp.src(_.folder(paths.app) + '/index.html')
         .pipe($.stringReplace('{version}', version))
         .pipe(gulp.dest(_.folder(paths.dist)));
 };
