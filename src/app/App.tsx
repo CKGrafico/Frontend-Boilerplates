1658bc26d97665390a78cfd5e8ed67d97535fab3
Quique Fdez Guerra <quique@ckgrafico.com>
2019-04-07 22:44:23 +0200

    Add assets and fix linting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
