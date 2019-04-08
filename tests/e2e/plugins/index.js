a6df24f44fd661e259da33b24c8c470ae5016974
Quique Fdez Guerra <quique@ckgrafico.com>
2019-04-08 23:52:10 +0200

    Config cypress

---
+++ tests/e2e/plugins/index.js
@@ -0,0 +1,8 @@
+module.exports = (on, config) => {
+  return Object.assign({}, config, {
+    fixturesFolder: 'tests/e2e/fixtures',
+    integrationFolder: 'tests/e2e/specs',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    videosFolder: 'tests/e2e/videos'
+  })
+}
---
+++ tests/e2e/specs/app.spec.js
@@ -0,0 +1,5 @@
+describe('My First Test', function() {
+  it('Visits the Kitchen Sink', function() {
+    cy.visit('/')
+  })
+})
