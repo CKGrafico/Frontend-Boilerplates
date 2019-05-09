d322e9aefb38ba04f5310ad873ab923b9906e03e
Quique Fdez Guerra <quique@ckgrafico.com>
2019-05-09 10:39:35 +0200

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'ts',
+    'tsx'
+  ],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/**/*.test.(js|jsx|ts|tsx)|**/tests/unit/specs/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/',
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
+}
