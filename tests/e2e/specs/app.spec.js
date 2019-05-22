55ec6c5f9f48deca7871946371403c3c6313a04b
Quique Fdez Guerra <quique@ckgrafico.com>
2019-05-22 09:44:05 +0200

    Update app.spec.js

--- tests/e2e/specs/app.spec.js
+++ tests/e2e/specs/app.spec.js
@@ -1,5 +1,8 @@
 describe('My First Test', function() {
   it('Visits the Kitchen Sink', function() {
-    cy.visit('/')
+    cy.visit('/');
+    
+    // cy.get('.input').type('text').should('have.value', 'text');
+    // cy.get('h1').should('contain', 'inner text')
   })
 })
