d0df5022abcaef652371ea085380ca6c181b1e35
Quique Fdez Guerra <efernandez@plainconcepts.com>
2019-08-05 17:53:06 +0200

    Add comments

--- src/app/shared/example.hook.ts
+++ src/app/shared/example.hook.ts
@@ -1,6 +1,7 @@
 import { useExampleStore, ExampleStoreType, ExampleStoreState } from '../../store';
 
 export function useExample(): [ExampleStoreState, () => void] {
+  // You can use stores directly form components, this is only an example of hook
   const [state, dispatch] = useExampleStore();
 
   function incrementProperty1() {
