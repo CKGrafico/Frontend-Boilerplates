8f16bd537f72bec45bf7a620a85526f5474056dd
Quique Fdez Guerra <quique@ckgrafico.com>
2019-08-26 20:16:38 +0200

    Fix cy

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,15 +1,16 @@
-const wp = require('@cypress/webpack-preprocessor')
+const wp = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
   const options = {
-    webpackOptions: require('../../../webpack.config'),
-  }
-  on('file:preprocessor', wp(options))
+    webpackOptions: require('../../../webpack.config')
+  };
+
+  on('file:preprocessor', wp(options));
 
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
     screenshotsFolder: 'tests/e2e/screenshots',
     videosFolder: 'tests/e2e/videos'
-  })
-}
+  });
+};
