eecf1a1adbbb4829ecfe68c47dc2a85cc5b389e6
Quique Fdez Guerra <quique@ckgrafico.com>
2019-11-10 15:46:26 +0000

    Move to terser

--- vue.config.js
+++ vue.config.js
@@ -1,46 +1,32 @@
 const path = require('path');
-// const rules = require('require.all')('./tasks/rules');
-const plugins = require('require.all')('./tasks/plugins');
 
 module.exports = env => {
-  let environment = process.env.NODE_ENV;
-
-  // rules((name, rule) => rule(environment));
-  plugins((name, rule) => rule(environment));
-
-  return ({
+  return {
     lintOnSave: false,
     css: {
       loaderOptions: {
         sass: {
-          data: `@import "~@/styles/base/_variables.scss";`
+          prependData: `@import '~@/styles/base/_variables.scss';`
         }
       }
     },
-    configureWebpack: {
-      plugins: [],
-      optimization: {
-        splitChunks: {
-          cacheGroups: {
-            vendor: {
-              chunks: 'all',
-              test: path.resolve(__dirname, 'node_modules'),
-              name: 'vendor',
-              enforce: true,
-            },
-          },
-        },
-      },
-      resolve: {
-        alias: {
-          'styles': path.join(__dirname, 'src/styles'),
-          'assets': path.join(__dirname, 'src/assets'),
-          '~': path.join(__dirname, 'src/app')
-        }
-      }
-    },
-    chainWebpack: config => {
-        config.optimization.minimizer([plugins.uglify]);
+    configureWebpack: config => {
+      const alias = {
+        styles: path.join(__dirname, 'src/styles'),
+        assets: path.join(__dirname, 'src/assets'),
+        '~': path.join(__dirname, 'src/app')
+      };
+
+      const terserOptions = {
+        keep_classnames: true,
+        keep_fnames: true
+      };
+
+      config.resolve.alias = { ...config.resolve.alias, ...alias };
+      config.optimization.minimizer[0].options.terserOptions = {
+        ...config.optimization.minimizer[0].options.terserOptions,
+        ...terserOptions
+      };
     }
-  })
-}
+  };
+};
