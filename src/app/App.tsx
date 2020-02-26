b9ed549ab7445db6bc267581c4dab7b54f271bc5
Quique Fdez Guerra <quique@ckgrafico.com>
2020-02-26 15:14:52 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { containerBuilder } from './app.container';
+import '../styles/app.scss';
 import './app.i18n';
 import AppRouter from './AppRouter';
 import { Header } from './layout';
