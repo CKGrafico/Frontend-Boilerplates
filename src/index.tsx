94b3bff25fe1f34edf203f070e9a342c4468a61f
Quique Fdez Guerra <quique@ckgrafico.com>
2020-02-26 15:15:05 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'reflect-metadata';
 import App from './app/App';
-import './styles/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
