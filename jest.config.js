5c16ddb66044f2422515152b3e847e92f4c68135
Quique Fdez Guerra <efernandez@plainconcepts.com>
2020-06-30 11:51:27 +0200

    Update config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
 module.exports = {
-  moduleFileExtensions: [
-    'js',
-    'json'
-  ],
-  transformIgnorePatterns: [
-    '/node_modules/'
-  ],
+  moduleFileExtensions: ['js', 'json'],
+  transformIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
-  testMatch: [
-    '**/**/*.test.(js)|**/tests/unit/*.(js)'
-  ],
+  testMatch: ['**/**/*.test.(js)|**/tests/unit/*.(js)'],
   testURL: 'http://localhost/'
 };
