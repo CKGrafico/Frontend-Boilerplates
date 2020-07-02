479db9cbc71908bbaa36821de19560f606d50870
ckgrafico <quique@ckgrafico.com>
2020-07-02 18:34:07 +0200

    Update styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,36 +16,36 @@ function Home() {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
-            <Link className={clsx("button button--outline button--secondary button--lg", styles.getStarted)} to={useBaseUrl("docs/")}>
-              <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/logo/logo.png" alt="JavaScript Boilerplate" width="150" />
+            <Link className={styles.logo} to={useBaseUrl("docs/")}>
+              <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/logo/logo.png" alt="Logo Boilerplate" width="150" />
             </Link>
           </div>
         </div>
       </header>
       <main>
         <p className={styles.logos} align="center">
-          <Link to={useBaseUrl("docs/")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/spa.png" alt="JavaScript Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/vue")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/vue")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/vue.png" alt="Vue Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/react")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/react")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/react.png" alt="React Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/ts")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/ts")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/ts.png" alt="TypeScript Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/phaser")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/phaser")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/phaser.png" alt="Phaser Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/cypress")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/cypress")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/cypress.png" alt="Cypress Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/nuxt")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/nuxt")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/nuxt.png" alt="Nuxt Boilerplate" width="150" />
           </Link>
-          <Link to={useBaseUrl("docs/node-ts")}>
+          <Link className={styles.logo} to={useBaseUrl("docs/node-ts")}>
             <img src="https://raw.githubusercontent.com/CKGrafico/Frontend-Boilerplates/docs/resources/techs/nodets.png" alt="Node and TS Boilerplate" width="150" />
           </Link>
         </p>
